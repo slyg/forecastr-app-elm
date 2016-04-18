817e30820ff202f7fb330e37530f2ecae010e6f2
Sylvain Faucherand <syl.faucherand@gmail.com>
2016-04-18 19:45:55 +0100

    Init a Json decoder func

--- src/elm/WebApi.elm
+++ src/elm/WebApi.elm
@@ -8,13 +8,23 @@ import Effects exposing (Effects)
 
 import ActionTypes exposing (Action(..))
 
-token : String
-token =
-  "3b080a643fbe01608d05a365e2b49996"
+forecastUrl : String -> String
+forecastUrl query =
+  String.concat
+    [ "http://api.openweathermap.org/data/2.5/forecast"
+    , "?q="
+    , query
+    , "&appid="
+    , "3b080a643fbe01608d05a365e2b49996"
+    ]
+
+forecast : Json.Decoder (String)
+forecast =
+  ("cod" := Json.string)
 
 requestForecast : String -> Effects Action
 requestForecast query =
-  Http.get ("cod" := Json.string) (String.concat ["http://api.openweathermap.org/data/2.5/forecast?q=", query, "&appid=", token])
+  Http.get forecast (forecastUrl query)
     |> Task.toMaybe
     |> Task.map UpdateForecast
     |> Effects.task

5dd0f3e7b982c523299f8c15351cd729f6c0de71
Sylvain Faucherand <syl.faucherand@gmail.com>
2016-04-18 17:25:32 +0100

    Proper ajax call with elm

--- src/Main.elm
+++ src/Main.elm
@@ -1,27 +1,74 @@
-import StartApp.Simple as StartApp
-import Html exposing (text, div)
+import StartApp
+import Html exposing (Html, text, div, button)
 import Html.Events exposing (onClick)
 
-main =
+import Signal exposing (Address)
+import Effects exposing (Effects, Never)
+import Http
+import Json.Decode as Json exposing ((:=))
+import Task exposing (..)
+
+app =
   StartApp.start
-    { view = view
+    { init = init
+    , view = view
     , update = update
-    , model = model
+    , inputs = []
     }
 
-model =
-  { nextId = 0
+main =
+  app.html
+
+port tasks : Signal (Task.Task Never ())
+port tasks =
+  app.tasks
+
+type alias Model =
+  { nextId : Int
+  , ip : String
   }
 
-type Action = Increment
+init : (Model, Effects Action)
+init =
+  ( { nextId = 0
+    , ip = "Unknown"
+    }
+  , Effects.none
+  )
 
+model = init
+
+type Action
+  = NoOp
+  | RequestIP
+  | UpdateIP (Maybe String)
+
+update : Action -> Model -> ( Model, Effects Action )
 update action model =
-
   case action of
-    Increment ->
-      { model |
-          nextId = model.nextId + 1
-      }
 
+    RequestIP ->
+      (model, requestIP "http://jsonip.com")
+
+    UpdateIP ip ->
+      ( { model | ip = (Maybe.withDefault "No response" ip) }
+        , Effects.none
+      )
+
+    NoOp ->
+      (model, Effects.none)
+
+
+view : Address Action -> Model -> Html
 view address model =
-  div [ onClick address Increment ] [ text ("Increment Id: " ++ (toString model.nextId)) ]
+  div []
+    [ button [ onClick address RequestIP ] [ text "Get Ip" ]
+    , div [] [ text model.ip ]
+  ]
+
+requestIP : String -> Effects Action
+requestIP url =
+  Http.get ("ip" := Json.string) url
+    |> Task.toMaybe
+    |> Task.map UpdateIP
+    |> Effects.task

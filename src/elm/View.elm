61b1604401dd889ce7dc3183cff61360aa694d20
Sylvain Faucherand <syl.faucherand@gmail.com>
2016-04-20 00:04:33 +0100

    Add some basic style

--- src/elm/View.elm
+++ src/elm/View.elm
@@ -1,16 +1,33 @@
 module View where
 
 import StartApp
+import String
 import Html exposing (Html, text, div, button)
 import Html.Events exposing (onClick)
+import Html.Attributes exposing (style)
 import Signal exposing (Address)
 
 import ActionTypes exposing (Action(..))
 import Store exposing (Model)
 
+lineStyle =
+  style
+    [ ("padding", "10px 10px 0")
+    ]
+
 view : Address Action -> Model -> Html
 view address model =
   div []
-    [ button [ onClick address RequestForecast ] [ text "Get code" ]
-    , div [] [ text model.city.name ]
+    [ div [ lineStyle ]
+      [ button [ onClick address RequestForecast ] [ text "Get city" ]
+      ]
+    , div [ lineStyle ]
+      [ text
+        ( String.concat
+          [ model.city.name
+            , " ("
+            , model.city.country, ")"
+          ]
+        )
+      ]
   ]
